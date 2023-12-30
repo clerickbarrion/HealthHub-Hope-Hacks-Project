6a4d8026abc3ef578da60fb5b7418939d346a6ef
clerickbarrion <clerickbarrion@gmail.com>
2023-12-29 23:43:19 -0700

    added ping and whisper

--- src/utils/chat.js
+++ src/utils/chat.js
@@ -8,12 +8,18 @@ function connectChat(server) {
     io.on('connection', socket=>{
         let user
         let time
+        let hex
 
-        socket.on('get user', name=>name ? user = name : user = `Anon ${socket.id.substring(0,5)}`)
-        socket.on('chat message', msg=>io.emit('chat message', `<mark>${user}:</mark> ${msg}`))
-        socket.on('join', ()=>io.emit('join', `<mark>${user}</mark> has entered the chat`))
-        socket.on('disconnect',()=>io.emit('leave', `<mark>${user}</mark> has left the chat`))
-        socket.on('leave',()=>io.emit('leave', `<mark>${user}</mark> has left the chat`))
+        socket.on('get user', cred =>{
+            cred.name ? user = cred.name : user = `Anon ${socket.id.substring(0,5)}`
+            cred.hex ? hex = cred.hex : hex = '000000'
+        })
+        socket.on('chat message', msg=>io.emit('chat message', `<mark style="color:#${hex}">${user}:</mark> ${msg}`))
+        socket.on('join', ()=>io.emit('join', `<mark style="color:#${hex}">${user}</mark> has entered the chat`))
+        socket.on('disconnect',()=>io.emit('leave', `<mark style="color:#${hex}">${user}</mark> has left the chat`))
+        socket.on('leave',()=>io.emit('leave', `<mark style="color:#${hex}">${user}</mark> has left the chat`))
+        socket.on('ping', ping=>io.emit('ping',ping))
+        socket.on('whisper', whisper=>io.emit('whisper',whisper))
         socket.on('typing',()=>{
             if (!typers.includes(user)) { typers.push(user) }
             io.emit('typing', typers)
