c80344bf784dbce21107571b8f6ab5be6138ded9
cmatthews15 <chastitydmatthews@gmail.com>
2024-01-08 13:18:16 -0500

    finish

--- public/js/mentalHealth.js
+++ public/js/mentalHealth.js
@@ -1,52 +1,44 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
-  });
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, options);
+});
 
 
 const feelingInputElem= document.getElementById('feeling')
 const adviceHeader = document.getElementById('resourceHeading')
-  const adviceOutput1 = document.getElementById('resourceList1')
-  const adviceOutput2 = document.getElementById('resourceList2')
-  const adviceOutput3 = document.getElementById('resourceList3')
-  const agreeButton = document.getElementById('agree')
+const adviceOutput1 = document.getElementById('resourceList1')
+const adviceOutput2 = document.getElementById('resourceList2')
+const adviceOutput3 = document.getElementById('resourceList3')
+const agreeButton = document.getElementById('agree')
 
-agreeButton.addEventListener('click', async ()=>{
 
-const list = await resourceDisplay()
-console.log(list)
-for (let i = 0; i < list.length ; i++){
-  if(feelingInputElem.value === list[i].feeling){
-  adviceHeader.innerText='Here are tips based on how you\'re feeling:'
-adviceOutput1.innerText= list[i].advice1;
-adviceOutput2.innerText= list[i].advice2;
-adviceOutput3.innerText= list[i].advice3;
+agreeButton.addEventListener('click', async () => {
+  const list = await resourceDisplay();
+  console.log(list);
+  let feelingFound = false; // keep track if a match is found
+  for (let i = 0; i < list.length; i++) {
+    if (feelingInputElem.value.toUpperCase() === list[i].feeling.toUpperCase()) {
+      adviceHeader.innerText = 'Here are tips based on how you\'re feeling:';
+      adviceOutput1.innerText = list[i].advice1;
+      adviceOutput2.innerText = list[i].advice2;
+      adviceOutput3.innerText = list[i].advice3;
+      feelingFound = true; // true when a match is found
+      break; // exit the loop since we found a match
+    }
+  }
+  if (!feelingFound) { // if no match is found
+    adviceHeader.innerText = 'Please select another feeling';
+  }
+});
 
+async function resourceDisplay() {
+  return fetch(`${window.location.origin}/feeling`)
+    .then(res => res.json())
+    .then(list => {
+      return list
+    });
 }
-}
-
-})
-
-  async function resourceDisplay(){
-
-return fetch(`${window.location.origin}/feeling`)
-.then(res=> res.json())
-.then(list=>{
-  return list
-
-
- 
-})}
-  
-
-
-
-  // create drop down menu of feelings that narrows the list to match, displays no relsults in drop menu if none are in list
-//takes selected feeling and returns matching value from object, get resources trigger modal
-// when id.agree is clicked. use feelings.inner text to s
-//
-
 
 
 let i = 0;
@@ -81,4 +73,6 @@ function type() {
 }
 
 type();
+
+
 
